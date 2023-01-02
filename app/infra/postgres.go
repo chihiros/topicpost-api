679de3f35c474fd9c8691d1634fdcd15ff89c2f1
chihiros <szrkw2ui2a@gmail.com>
2023-01-03 05:55:54 +0900

    fix: ifの処理変更

--- app/infra/postgres.go
+++ app/infra/postgres.go
@@ -11,8 +11,8 @@ import (
 )
 
 func NewPostgresConnection() (*ent.Client, error) {
-	var DB_URL string
-	if _, exist := os.LookupEnv("DATABASE_URL"); !exist {
+	DB_URL, exist := os.LookupEnv("DATABASE_URL")
+	if !exist {
 		DB_URL = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
 			os.Getenv("DB_USER"),
 			os.Getenv("DB_PASSWORD"),
@@ -20,8 +20,6 @@ func NewPostgresConnection() (*ent.Client, error) {
 			os.Getenv("DB_PORT"),
 			os.Getenv("DB_NAME"),
 		)
-	} else {
-		DB_URL = os.Getenv("DATABASE_URL")
 	}
 
 	client, err := ent.Open("postgres", DB_URL)
